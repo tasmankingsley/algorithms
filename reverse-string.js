2eaf5f97f1fbe34778fec476b34216a3bafa274e
tasmankingsley <tasman.kingsley@gmail.com>
2022-08-12 23:52:20 +0900

    update

--- reverse-string.js
+++ reverse-string.js
@@ -1,7 +1,21 @@
 let str = 'string';
 
+// Option 1
+
+// function reverse_string(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// Option 2
+
 function reverse_string(str) {
-    return str.split('').reverse().join('');
+    let rev_str = '';
+
+    for (let i = str.length  - 1; i >= 0; i--) {
+        rev_str += str[i];
+    }
+    
+    return rev_str;
 }
 
 console.log(reverse_string(str));
